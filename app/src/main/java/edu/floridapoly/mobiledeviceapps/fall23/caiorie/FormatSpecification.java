b9d1bb8dcd9fcce5ee3fe241944c7f8b6f9d417d
snohoho <budderlod@gmail.com>
2023-11-02 20:07:33 -0400

    itl commit

---
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/caiorie/FormatSpecification.java
@@ -0,0 +1,14 @@
+package edu.floridapoly.mobiledeviceapps.fall23.caiorie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormatSpecification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.format_specification);
+    }
+}
